7e84a578443d06e36797636b63e7a62d55400d50
quantumtom <cornyn@gmail.com>
2018-02-27 13:03:58 -0800

    Fix boot load order.

--- exportPlugin.js
+++ exportPlugin.js
@@ -6,12 +6,13 @@ module.exports = class ExportPlugin {
             const headerTemplate = `define("${compiler.options.output.library}", [], function() { return { start: function(options) { return new Promise(function(resolve) {`
             const importTemplate = `System.import("{{replacement}}")`
             const mainTemplate = `.then(function () { return System.import("{{replacement}}")})`
-            const footerTemplate = `.then(function(mod) { resolve(mod.default.start({el: options.el, config: options.config}))}); }); } }; });`
+            const footerTemplate = `.then(function(mod) { resolve(mod.default.start({el: options.el, config: options.config}))})`
             compilation.chunks.forEach(chunk => {
                 let bootloader = headerTemplate
                 bootloader += importTemplate.replace('{{replacement}}', compiler.options.output.publicPath + chunk.files[0]);
-                bootloader += mainTemplate.replace('{{replacement}}', compiler.options.output.publicPath + chunk.files[0]);
                 bootloader += footerTemplate;
+                bootloader += mainTemplate.replace('{{replacement}}', compiler.options.output.publicPath + chunk.files[0]);
+                bootloader += `; }); } }; });`;
                 const hash = crypto.createHash('md5');
                 hash.update(bootloader);
                 // compilation.assets[`${chunk.name}-bootloader.js`] = {
