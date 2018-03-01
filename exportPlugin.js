2f8e49e67b7f1bdebee94f0fb7152965721a1775
quantumtom <cornyn@gmail.com>
2018-03-01 11:50:55 -0800

    Update webpack export plugin output.

--- exportPlugin.js
+++ exportPlugin.js
@@ -3,22 +3,18 @@ const crypto = require('crypto');
 module.exports = class ExportPlugin {
     apply(compiler) {
         compiler.plugin('emit', (compilation, done) => {
-            const headerTemplate = `define("${compiler.options.output.library}", [], function() { return { start: function(options) { return new Promise(function(resolve) {`
-            const importTemplate = `System.import("{{replacement}}")`
-            const mainTemplate = `.then(function () { return System.import("{{replacement}}")})`
-            const footerTemplate = `.then(function(mod) { resolve(mod.default.start({el: options.el, config: options.config}))})`
+            const headerTemplate = `define("${compiler.options.output.library}", [], function() { return { start: function(options) { return new Promise(function(resolve) {`;
+            const importTemplate = `System.import("{{replacement}}")`;
+            const footerTemplate = `.then(function(mod) { resolve(mod.default.start({el: options.el, config: options.config}))})`;
+            const mainTemplate = `.then(function () { return System.import("{{replacement}}")})`;
             compilation.chunks.forEach(chunk => {
-                let bootloader = headerTemplate
+                let bootloader = headerTemplate;
                 bootloader += importTemplate.replace('{{replacement}}', compiler.options.output.publicPath + chunk.files[0]);
                 bootloader += footerTemplate;
                 bootloader += mainTemplate.replace('{{replacement}}', compiler.options.output.publicPath + chunk.files[0]);
                 bootloader += `; }); } }; });`;
                 const hash = crypto.createHash('md5');
                 hash.update(bootloader);
-                // compilation.assets[`${chunk.name}-bootloader.js`] = {
-                //     source: _ => bootloader,
-                //     size: _ => bootloader.length
-                // };
                 compilation.assets[`${chunk.name}-bootloader-${hash.digest("hex")}.js`] = {
                     source: _ => bootloader,
                     size: _ => bootloader.length
@@ -28,4 +24,3 @@ module.exports = class ExportPlugin {
         })
     }
 };
-
