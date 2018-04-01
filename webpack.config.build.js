534041a96f05b215644e68a7c0dd32a612e4bad4
quantumtom <cornyn@gmail.com>
2018-04-01 12:42:31 -0700

    Remove public directory. Et c.

---
+++ webpack.config.build.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpackConfig = require('./webpack.config');
+
+module.exports = merge(webpackConfig, {
+
+    devtool: 'cheap-module-source-map',
+
+    // plugins: [
+    //     new webpack.optimize.CommonsChunkPlugin({
+    //         names: ['index', 'main']
+    //     }),
+    //     new CleanWebpackPlugin(['dist'])
+    // ],
+    output: {
+        filename: '[name]-[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'pfc',
+        libraryTarget: 'amd',
+        publicPath: 'https://addons.redbull.com/us/playingforchange/'
+    }
+
+});
