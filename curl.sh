66a8e7e7f005281cc7c389a6cc555097bff31d9f
quantumtom <cornyn@gmail.com>
2018-04-24 08:29:57 -0700

    Update clone command.

--- curl.sh
+++ curl.sh
@@ -1,1 +1,1 @@
-curl -L -o master.zip https://github.com/quantumtom/phasetwo/archive/master.zip && unzip master.zip && rm master.zip && mv ./webpack-boilerplate-master/{.,}* ./ && rm -r ./webpack-boilerplate-master
+curl -L -o master.zip https://github.com/quantumtom/phasetwo/archive/master.zip && unzip master.zip && rm master.zip && mv ./phasetwo-master/{.,}* ./ && rm -r ./phasetwo-master
