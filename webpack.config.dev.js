7704e1627c2b8be44f1e0080845ea1f4b7125004
quantumtom <cornyn@gmail.com>
2018-05-18 11:28:28 -0700

    Update library name.

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -3,12 +3,14 @@ const webpackConfig = require('./webpack.config');
 
 module.exports = merge(webpackConfig, {
 
+    mode: 'development',
+
     devtool: 'eval',
 
     output: {
         pathinfo: true,
         publicPath: '/',
-        library: 'roxrite-lib',
+        library: 'phase3-DevLib',
         libraryTarget: 'umd',
         filename: '[name].js'
     }
