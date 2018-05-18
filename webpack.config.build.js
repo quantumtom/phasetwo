7732e4ce8a5e041ebb4c625e66f2c4752bfc7828
quantumtom <cornyn@gmail.com>
2018-05-18 11:29:06 -0700

    Update library name, publicpath.

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,25 +1,25 @@
 const path = require('path');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 const webpackConfig = require('./webpack.config');
 
 module.exports = merge(webpackConfig, {
 
+    mode: 'production',
+
     devtool: 'cheap-module-source-map',
 
     plugins: [
-        new webpack.optimize.CommonsChunkPlugin({
+        new webpack.optimize.SplitChunksPlugin({
             names: ['index', 'main']
-        }),
-        new CleanWebpackPlugin(['dist'])
+        })
     ],
     output: {
         filename: '[name]-[hash].js',
         path: path.resolve(__dirname, 'dist'),
-        library: 'pfc',
+        library: 'phase3-build-lib',
         libraryTarget: 'amd',
-        publicPath: 'https://addons.redbull.com/us/playingforchange/'
+        publicPath: 'https://addons.redbull.com/us/phasetwo/dist/'
     }
 
 });
