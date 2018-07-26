2f20fe4b7f5b30b9e88fa91f7f4894fabaedf0f6
Justin Nance <>
2018-07-26 13:43:22 -0700

    Initial styling for new template

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -12,7 +12,7 @@ module.exports = merge(webpackConfig, {
         new CleanWebpackPlugin(['dist/*']),
         new webpack.optimize.SplitChunksPlugin({
             filename: 'index.js',
-            name: 'reign',
+            name: 'redbulletin',
             automaticNameDelimiter: '-',
             chunks: 'all',
             minChunks: 2,
@@ -22,9 +22,9 @@ module.exports = merge(webpackConfig, {
     output: {
         filename: '[name]-[hash].js',
         path: path.resolve(__dirname, 'dist'),
-        library: 'example-prod-lib',
+        library: 'rebulletin-lib',
         libraryTarget: 'amd',
-        publicPath: 'https://addons.redbull.com/us/example/'
+        publicPath: 'https://addons.redbull.com/us/redbulletin/'
     }
 
 });
